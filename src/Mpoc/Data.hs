e8f9f49cbac2b47059648a1a998079e4bdd3ade7
Bjørn Nordbø <bn@strangedays.no>
2016-09-27 18:35:15 +0200

    ResourceT and MBC magic seems ok

--- src/Mpoc/Data.hs
+++ src/Mpoc/Data.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE UndecidableInstances       #-}
 
 import Control.Lens
+import Control.Monad.Base
 import Control.Monad.IO.Class
-import Control.Monad.Trans.AWS
 import Control.Monad.Reader
+import Control.Monad.Trans.AWS
+import Control.Monad.Trans.Control
 import Data.Conduit
 import Mpoc.Types
 import Network.AWS.Data
@@ -12,18 +17,30 @@ import Network.AWS.Types
 import System.IO
 
 
-newtype Data a = Data { unwrap :: ReaderT AuthEnv IO a }
+newtype Data a = Data { unwrap :: ReaderT Env IO a }
   deriving ( Functor
            , Applicative
            , Monad
            , MonadIO
-           , MonadReader AuthEnv
+           , MonadReader Env
            )
 
-runData :: MonadIO m => AuthEnv -> Data a -> m a
+instance MonadBase IO Data where
+  liftBase = liftIO
+
+instance MonadBaseControl IO Data where
+  type StM Data a = StM (ReaderT Env IO) a
+  liftBaseWith f  = Data $ liftBaseWith $ \run -> f (run . unwrap)
+  restoreM        = Data . restoreM
+
+runData :: MonadIO m => Env -> Data a -> m a
 runData e d = liftIO $ runReaderT (unwrap d) e
 
+-- env' <- newEnv Ireland (FromProfile "mpoc_devel")
 ---
 
 test :: Data [Fragment]
-test = undefined
+test = do
+  env <- ask
+  runResourceT . runAWST env . within Ireland $ do
+    return []
